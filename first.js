2455b0bb9d9c1c289e0b178c693ae67553b50876
serhatyuce <serhatyuce.info@gmail.com>
2022-07-24 23:16:28 +0300

    js

--- first.js
+++ first.js
@@ -1,2 +1,16 @@
-console.log("Serhat Yüce");
+/*let kur = `SİZDEBİZİMÇEKOSLAVAKYALILAŞTIRAMADIKLARIMIZDANMISINIZ?`
+console.log(kur);
+kur = `${kur[0].toUpperCase()}${kur.slice(1).toLowerCase()}`
+console.log(kur);
+console.log(kur.search('ç'));
+console.log(kur.search('def')); 
 
+let dom = `serhatyuce.info@gmail.com`
+console.log(dom);
+dom = `${dom[0].toLowerCase()}${dom.slice(1).toUpperCase()}`
+console.log(dom)
+console.log(dom.search('@'));
+console.log(dom.slice(16)); 
+
+var isim = prompt("Adınız nedir?");
+console.log("Merhaba, " + isim); */
