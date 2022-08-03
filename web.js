4a1d9ad12ae93da7508b483aff885badcb81a34f
serhatyuce <serhatyuce.info@gmail.com>
2022-08-03 16:16:55 +0300

    removed

--- web.js
+++ web.js
@@ -1,2 +1,18 @@
 let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+document.body.innerHTML = "<h1>Today's date is " + d + "</h1>" //import text 
+
+/*let d = new Date();
+
+
+function date(){
+ functionName();
+	document.getElementById('date').innerHTML = "Mighthlk";
+}
+
+function functionName() {  
+	confirm("You");
+}
+
+function onload(){
+document.getElementById('h2').innerHTML = "<h1>Today's date is " + new Date() + "</h1>" //import text 
+} */
